06bc90215639361cd6f3e3b1db381df20363ba7f
dranchuk.roma <roman2003dranchuk@gmail.com>
2023-02-22 23:04:47 +0100

    Finished HTML structure

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+$(document).ready(function(){
+    // $('#js-cardImg').hover(function(){
+    //     $(this).children().addClass('cardHoverTrue');
+    // });
+    // $('#js-cardImg').mouseleave(function(){
+    //     $(this).children().removeClass('cardHoverTrue');
+    // });
+    $('.cardImg').hover(function(){
+        $(this).children().stop().animate({ opacity: 1 }, 200);
+         }, function() {
+        $(this).children().stop().animate({ opacity: 0 }, 200);
+        });
+
+
+
+});
