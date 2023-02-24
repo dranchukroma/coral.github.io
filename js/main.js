18d0fcb4b8384afc7f480c070b8d8ceb37cbd5c1
dranchuk.roma <roman2003dranchuk@gmail.com>
2023-02-24 23:45:01 +0100

    Added hover to linkCards, also added cursor pointer for required elements

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
-    // $('#js-cardImg').hover(function(){
-    //     $(this).children().addClass('cardHoverTrue');
-    // });
-    // $('#js-cardImg').mouseleave(function(){
-    //     $(this).children().removeClass('cardHoverTrue');
-    // });
-    $('.cardImg').hover(function(){
+    hoverCards('.cardImg');
+    hoverLinks('.box', '.hoverLink');
+});
+
+function hoverCards(hoverElement){
+    $(hoverElement).hover(function(){
         $(this).children().stop().animate({ opacity: 1 }, 200);
-         }, function() {
+        }, function() {
         $(this).children().stop().animate({ opacity: 0 }, 200);
         });
+}
 
-
-
-});
+function hoverLinks(hoverElement, hoverChild){
+    $(hoverElement).hover(function(){
+        $(this).children(hoverChild).stop().animate({ opacity: 1 }, 200);
+        }, function() {
+        $(this).children(hoverChild).stop().animate({ opacity: 0.7 }, 200);
+    });
+}
