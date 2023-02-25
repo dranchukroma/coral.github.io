613947656ad4f0dcf3d7ba0fa789433d94920015
dranchuk.roma <roman2003dranchuk@gmail.com>
2023-02-25 17:45:52 +0100

    Added navigation animation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     hoverCards('.cardImg');
     hoverLinks('.box', '.hoverLink');
+    navigation('.navigation');
 });
 
 function hoverCards(hoverElement){
@@ -16,5 +17,22 @@ function hoverLinks(hoverElement, hoverChild){
         $(this).children(hoverChild).stop().animate({ opacity: 1 }, 200);
         }, function() {
         $(this).children(hoverChild).stop().animate({ opacity: 0.7 }, 200);
+    });
+}
+
+// Navigation
+function navigation(navigationElement){
+    //Hide and show navigation on wheel
+    $(document).on('wheel', function(e){
+        if(e.originalEvent.wheelDelta >=0){
+            $(navigationElement).css({'top': '0px'});
+        }
+        else if($(window).scrollTop() > $('.header').height()){
+            $(navigationElement).css({'top': ('-' + ($(navigationElement).height() + 30) + 'px')});
+        }
+    })
+    //Changing background-color
+    $(window).scroll(function(){
+        ($(window).scrollTop() > 0) ? $(navigationElement).addClass('navigationBG') : $(navigationElement).removeClass('navigationBG');
     });
 }
