feb23cf96e35639780812e5a46bc9564a375bb94
dranchuk.roma <roman2003dranchuk@gmail.com>
2023-03-01 16:20:16 +0100

    Header was changed, added animation for header changing

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     hoverLinks('.box', '.hoverLink');
     navigation('.navigation');
+    changingHeader();
+
 });
 
 function hoverLinks(onHoverElement, choverElement){
@@ -26,4 +28,200 @@ function navigation(navigationElement){
     $(window).scroll(function(){
         ($(window).scrollTop() > 0) ? $(navigationElement).addClass('navigationBG') : $(navigationElement).removeClass('navigationBG');
     });
+}
+
+
+
+
+function changingHeader(){
+    $('.dot').on('click', function(){
+        var checkElement = $(this).attr("id")
+        if(checkElement == 'js-dot-1'){
+            changeToStyle1();
+        }
+        else if(checkElement == 'js-dot-2'){
+            changeToStyle2();
+        }
+        else if(checkElement == 'js-dot-3'){
+            changeToStyle3();
+        }
+    });
+}
+function changeToStyle1(){
+    //Changing dots status Active/noActive
+    $('#js-dot-1').addClass('dotActive');
+    $('#js-dot-2').removeClass('dotActive');
+    $('#js-dot-3').removeClass('dotActive');
+    //Changing background for paintElement
+    $('#js-paintColorToChange').animate({
+        backgroundColor: 'rgb(77, 75, 80)',
+    }, 300)
+    // Changing background-image
+    $('.imgBackground').animate({
+        opacity: '0',
+    }, 300)
+    setTimeout(function(){
+        $('.imgBackground').css({
+            'background': 'url("../img/mainPage/headerStyle1.png") center',
+            'background-size': 'cover',
+        });
+    }, 300)
+    $('.imgBackground').animate({
+        opacity: '1',
+    });
+    //Changing text
+    $('#js-firstTitleChange').animate({
+        opacity: '0',
+    }, 300);
+    $('#js-secondTitleChange').animate({
+        opacity: '0',
+    }, 300);
+    $('#js-modelToChange').animate({
+        opacity: '0',
+    }, 300);
+    $('#js-colorToChange').animate({
+        opacity: '0',
+    }, 300);
+    setTimeout(function(){
+        $('.headerFirstTitle').css('background-color', 'white');
+        $('.headerSecondTitle').css('background-color', 'white');
+        $('.changingFirstTitle').css('width', '376px');
+        $('.headerFirstTitle').css('width', '714px');
+        $('.headerSecondTitle').css('width', '649px');
+        $('#js-firstTitleChange').text('Get Ready to Take on Any Challenge');
+        $('#js-secondTitleChange').text('Our Fashion-Forward Sportswear!');
+        $('#js-modelToChange').text('19-3901 TCX');
+        $('#js-colorToChange').text('Magnet');
+    }, 300)
+    $('#js-firstTitleChange').animate({
+        opacity: '1',
+    }, 300);
+    $('#js-secondTitleChange').animate({
+        opacity: '1',
+    }, 300);
+    $('#js-modelToChange').animate({
+        opacity: '1',
+    }, 300);
+    $('#js-colorToChange').animate({
+        opacity: '1',
+    }, 300);
+}
+function changeToStyle2(){
+    //Changing dots status Active/noActive
+    $('#js-dot-1').removeClass('dotActive');
+    $('#js-dot-2').addClass('dotActive');
+    $('#js-dot-3').removeClass('dotActive');
+    //Changing background for paintElement
+    $('#js-paintColorToChange').animate({
+        backgroundColor: 'rgb(23, 74, 69)',
+    }, 300)
+    // Changing background-image
+    $('.imgBackground').animate({
+        opacity: '0',
+    }, 300)
+    setTimeout(function(){
+        $('.imgBackground').css({
+            'background': 'url("../img/mainPage/headerStyle2.png") center',
+            'background-size': 'cover',
+        });
+    }, 300)
+    $('.imgBackground').animate({
+        opacity: '1',
+    });
+    //Changing text
+    $('#js-firstTitleChange').animate({
+        opacity: '0',
+    }, 300);
+    $('#js-secondTitleChange').animate({
+        opacity: '0',
+    }, 300);
+    $('#js-modelToChange').animate({
+        opacity: '0',
+    }, 300);
+    $('#js-colorToChange').animate({
+        opacity: '0',
+    }, 300);
+    setTimeout(function(){
+        $('.headerFirstTitle').css('background-color', 'transparent');
+        $('.headerSecondTitle').css('background-color', 'transparent');
+        $('.changingFirstTitle').css('width', '259px');
+        $('.headerFirstTitle').css('width', '596px');
+        $('.headerSecondTitle').css('width', '558px');
+        $('#js-firstTitleChange').text('Elevate Your Athletic Style');
+        $('#js-secondTitleChange').text('Our Fashionable Sportswear');
+        $('#js-modelToChange').text('19-5230 TCX');
+        $('#js-colorToChange').text('Forest Biome');
+    }, 300)
+    $('#js-firstTitleChange').animate({
+        opacity: '1',
+    }, 300);
+    $('#js-secondTitleChange').animate({
+        opacity: '1',
+    }, 300);
+    $('#js-modelToChange').animate({
+        opacity: '1',
+    }, 300);
+    $('#js-colorToChange').animate({
+        opacity: '1',
+    }, 300);
+}
+function changeToStyle3(){
+    //Changing dots status Active/noActive
+    $('#js-dot-1').removeClass('dotActive');
+    $('#js-dot-2').removeClass('dotActive');
+    $('#js-dot-3').addClass('dotActive');
+    //Changing background for paintElement
+    $('#js-paintColorToChange').animate({
+        backgroundColor: 'rgb(84, 59, 54)',
+    }, 300)
+    // Changing background-image
+    $('.imgBackground').animate({
+        opacity: '0',
+    }, 300)
+    setTimeout(function(){
+        $('.imgBackground').css({
+            'background': 'url("../img/mainPage/headerStyle3.png") center',
+            'background-size': 'cover',
+        });
+    }, 300)
+    $('.imgBackground').animate({
+        opacity: '1',
+    });
+    //Changing text
+    $('#js-firstTitleChange').animate({
+        opacity: '0',
+    }, 300);
+    $('#js-secondTitleChange').animate({
+        opacity: '0',
+    }, 300);
+    $('#js-modelToChange').animate({
+        opacity: '0',
+    }, 300);
+    $('#js-colorToChange').animate({
+        opacity: '0',
+    }, 300);
+    setTimeout(function(){
+        $('.headerFirstTitle').css('background-color', 'white');
+        $('.headerSecondTitle').css('background-color', 'white');
+        $('.changingFirstTitle').css('width', '184px');
+        $('.headerFirstTitle').css('width', '536px');
+        $('.headerSecondTitle').css('width', '661px');
+        $('#js-firstTitleChange').text('Get Your Game On');
+        $('#js-secondTitleChange').text('Our Stylish Sportswear Collection!');
+        $('#js-modelToChange').text('19-1215 TCX');
+        $('#js-colorToChange').text('Shaved Chocolate');
+    }, 300)
+    $('#js-firstTitleChange').animate({
+        opacity: '1',
+    }, 300);
+    $('#js-secondTitleChange').animate({
+        opacity: '1',
+    }, 300);
+    $('#js-modelToChange').animate({
+        opacity: '1',
+    }, 300);
+    $('#js-colorToChange').animate({
+        opacity: '1',
+    }, 300);
+
 }
