a28ac63e25c57d4da39d4676c4591fd9906534af
dranchuk.roma <roman2003dranchuk@gmail.com>
2023-03-27 23:14:17 +0200

    Started work on basket, small correct and started making aboutUs page

---
+++ js/index.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+    navigation('.navigation');
+    addingToList('.addedCardsBasket', '.forSale', 'basket');
+    inputNull('#js-inputSearch', '.hiddenPopUp', '');
+    showHidePopUp('.cross-popUp', '.popUp-Basket-icon', '.popUp-Basket');
+    showHidePopUp('none', '#js-search', '.search-results');
+    displayList();
+});
+
+
+// Navigation
+function navigation(navigationElement){
+    //Hide and show navigation on wheel
+    $(document).on('wheel', function(e){
+        if(e.originalEvent.wheelDelta >=0){
+            $(navigationElement).css({'top': '0px'});
+        }
+        else if($(window).scrollTop() > $('.header').height()){
+            $(navigationElement).css({'top': ('-' + ($(navigationElement).height() + 30) + 'px')});
+        }
+    })
+}
+//Changing like and basket icon on click
+function addingToList(addTo, triggerClick, listName){
+    $(triggerClick).on('click', function(){
+        $(this).children().toggleClass('onClickIcon');
+        let item = makingCards(addTo);
+        console.log(item);
+
+        let listName = getList();
+        listName.push(item);
+        saveList(listName);
+    });
+}
+//Save list
+function saveList(listName){
+    localStorage.setItem(listName, JSON.stringify(listName));
+}
+//Get List
+function getList(){
+    let listName = localStorage.getItem('listName');
+    if(listName){
+        return JSON.parse(listName);
+    }
+    else{return []}
+}
+//Display list
+function displayList(){
+    let list = getList();
+    console.log(list);
+}
+
+
+
+
+//Change input value on 
+function inputNull(inputID, doIfHaveClass, inputVal){
+    if($(inputID).hasClass(doIfHaveClass)){
+        $(inputID).val(inputVal);
+    }
+}
+//Making cards
+function makingCards(addTo){
+    const card = $('<div class="cart"><div class="flexRow"><div class="cart-header"><p>Basic sports suit</p></div><div class="cross" id="js-deleteCross"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 6L6 18" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M6 6L18 18" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg></div></div><div class="cart-wrapper flexRow"><div class="cart-img"><img src="./img/mainPage/1stCardImg.png" width="135" height="168px" class="cart-image" alt=""></div><div class="item-info flexRow"><div class="item-articleNumber"><p>Article number: 2145995</p></div><div class="item-color"><p>green</p></div><div class="item-size">S</div><div class="item-counting"><div class="flexRow"><div class="item-minus"><div class="minus-line"></div></div><div class="item-count">1</div><div class="item-add"><div class="minus-line"></div><div class="plus-line"></div></div></div></div></div></div><div class="item-fullPrice"><p>Everything: <span class="item-price"></span> uah</p></div></div>')
+    card.find('#js-deleteCross').click(function(){
+        card.remove();
+    });
+    $(addTo).append(card);
+    return card;
+}
+
+//Show/Hide popUp on click
+function showHidePopUp(hideEl, showEl, popUpEl){
+    $(showEl).click(function(){
+        $(popUpEl).toggleClass('hiddenPopUp');
+    });
+    // $(hideEl).click(function(){
+    //     $(popUpEl).addClass('hiddenPopUp');
+    // });
+
+//??Close PopUp when user clicked on not PopUp
+    // $(document).on('click', function(e){
+    //     console.log(!$(popUpEl).hasClass("hiddenPopUp"));
+    //     if(!($(popUpEl).hasClass('hiddenPopUp'))){
+
+    //         if($(e.target).closest(popUpEl).length === 0) {
+    //         $(popUpEl).addClass('hiddenPopUp');
+    //         }
+    //     }
+    // });
+}
+//??Atributes for making cards
+// articleNumber, color, size, counts, price, image
--- js/mainPage.js
+++ js/mainPage.js
@@ -1,37 +1,7 @@
 $(document).ready(function(){
-    hoverLinks('.box', '.hoverLink');
-    navigation('.navigation');
-    changingOnClickHeader();
     timerToHeader(7000);
-    addingToFavorite();
-    addingToBasket();
-    search();
-    showHidePopUp('.cross-popUp', '.popUp-Basket-icon' , '.popUp-Basket');
-
-
+    hoverLinks('.box', '.hoverLink');
 });
-
-function hoverLinks(onHoverElement, choverElement){
-    $(onHoverElement).hover(function(){
-        $(this).children('a').children(choverElement).stop().animate({ opacity: 1 }, 200);
-        }, function() {
-        $(this).children('a').children(choverElement).stop().animate({ opacity: 0.7 }, 200);
-    });
-}
-
-// Navigation
-function navigation(navigationElement){
-    //Hide and show navigation on wheel
-    $(document).on('wheel', function(e){
-        if(e.originalEvent.wheelDelta >=0){
-            $(navigationElement).css({'top': '0px'});
-        }
-        else if($(window).scrollTop() > $('.header').height()){
-            $(navigationElement).css({'top': ('-' + ($(navigationElement).height() + 30) + 'px')});
-        }
-    })
-}
-
 //Timer to changing header
 function timerToHeader(changingTime){
     var changingInterval = setInterval(function(){
@@ -60,7 +30,6 @@ function timerToHeader(changingTime){
             }
         }, changingTime)
     });
-
 }
 //Changing header
 function changingOnClickHeader(){
@@ -255,51 +224,10 @@ function changeToStyle3(){
     }, 300);
 
 }
-//Changing like and basket icon on click
-function addingToFavorite(){
-    $('.Like').on('click', function(){
-        $(this).toggleClass('onClickIcon');
+function hoverLinks(onHoverElement, choverElement){
+    $(onHoverElement).hover(function(){
+        $(this).children('a').children(choverElement).stop().animate({ opacity: 1 }, 200);
+        }, function() {
+        $(this).children('a').children(choverElement).stop().animate({ opacity: 0.7 }, 200);
     });
 }
-function addingToBasket(){
-    $('.Basket').on('click', function(){
-        $(this).toggleClass('onClickIcon');
-        makingCards();
-    });
-}
-function search(){
-    $('#js-search').click(function(){
-        var noResults = $('<div class="popUp search-results"><p class="empty">The search yielded no results</p></div>');
-        $('.searchWrapper').append(noResults);
-
-        $('#js-inputSearch').keypress(function(){
-            //Searching between elements
-            console.log('searching in data bases');
-        });
-
-        $(document).on('click', function(e){
-            if($(e.target).closest('#js-searchWrapper').length === 0) {
-                $('.search-results').remove();
-            }
-        });
-    });
-}
-
-function makingCards(){
-    const card = $('<div class="cart"><div class="flexRow"><div class="cart-header"><p>Basic sports suit</p></div><div class="cross"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 6L6 18" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M6 6L18 18" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg></div></div><div class="cart-wrapper flexRow"><div class="cart-img"><img src="./img/mainPage/1stCardImg.png" width="135" height="168px" class="cart-image" alt=""></div><div class="item-info flexRow"><div class="item-articleNumber"><p>Article number: 2145995</p></div><div class="item-color"><p>green</p></div><div class="item-size">S</div><div class="item-counting"><div class="flexRow"><div class="item-minus"><div class="minus-line"></div></div><div class="item-count">1</div><div class="item-add"><div class="minus-line"></div><div class="plus-line"></div></div></div></div></div></div><div class="item-fullPrice"><p>Everything: <span class="item-price"></span> uah</p></div></div>')
-    $('.addedCardsBasket').append(card);
-}
-
-function showHidePopUp(hideEl, showEl, popUpEl){
-    console.log('Hello1');
-    $(showEl).click(function(){
-        $(popUpEl).toggleClass('hiddenPopUp');
-        console.log('Hello2');
-    });
-    $(hideEl).click(function(){
-        $(popUpEl).addClass('hiddenPopUp');
-        console.log('Hello2');
-    });
-}
-
-// articleNumber, color, size, counts, price, image
