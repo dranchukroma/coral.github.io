9a0cb2fd26cf13ad5065ddc663db8156ee2985c2
dranchuk.roma <roman2003dranchuk@gmail.com>
2023-04-04 23:05:49 +0200

    Added showing and hiding content on plus click

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -1,2 +1,21 @@
+$(document).ready(function(){
+    $('.show-content').click(function(){
+        $('.show-content').each(function(){
+            if($(this).parent().parent().find('.content-text').css('display') != 'none'){
+                $(this).parent().parent().find('.content-text').slideUp(300);
+                $(this).find('.plus-vertical').css('transform', 'translate(-50%, -50%) rotate(0deg)');
+            }
+        });
+        if($(this).parent().parent().find('.content-text').css('display') == 'none'){
+            $(this).parent().parent().find('.content-text').slideDown(300);
+            $(this).find('.plus-vertical').css('transform', 'translate(-50%, -50%) rotate(-90deg)');
+        }
+        else{
+            $(this).parent().parent().find('.content-text').slideUp(300);
+            $(this).find('.plus-vertical').css('transform', 'translate(-50%, -50%) rotate(0deg)');
+        }
+    });
+});
+
 
 
