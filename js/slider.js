9cc4330cfdbfdb44a565646d0c34844001383c32
dranchuk.roma <roman2003dranchuk@gmail.com>
2023-04-19 21:07:54 +0200

    Added animation for sliders arrow, and small correction of mainPage

--- js/slider.js
+++ js/slider.js
@@ -13,4 +13,15 @@ $(document).ready(function(){
       $('.next-slide').click(function() {
         $('.cards').slick('slickNext');
       });
+      sliderArrowAnimation();
 });
+function sliderArrowAnimation(){
+  $('.arrow').click(function(){
+      if($(this).hasClass('prev-slide')){
+          $(this).children('.animation-arrow').animate({left:'-8px'},100).delay(200).animate({left:"0px"},100);
+      }
+      if($(this).hasClass('next-slide')){
+          $(this).children('.animation-arrow').animate({right:'-8px'},100).delay(200).animate({right:"0px"},100);
+      }
+  });
+}
