555d259ec85998eaded58c55b38a02512fc07fac
dranchukroma <roman2003dranchuk@gmail.com>
2023-06-25 20:51:43 +0200

    About us finished

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
                 $(this).find('.plus-vertical').css('transform', 'translate(-50%, -50%) rotate(0deg)');
             }
         });
-        //If this element's button was clicked first time, show him
+        //If this element's button was clicked first time, show it
         if($(this).parent().parent().find('.content-text').css('display') == 'none'){
             $(this).parent().parent().find('.content-text').slideDown(300);
             $(this).find('.plus-vertical').css('transform', 'translate(-50%, -50%) rotate(-90deg)');
@@ -35,25 +35,132 @@ $(document).ready(function(){
             $('.aboutLink-block').removeClass('active');
             $('.aboutLink-block').find('.aboutLink-block-wrapper').animate({
                 opacity: '1',
-            }, 300);
+            }, 300, function(){
+                $(this).css('display', 'block');
+            });
             $('.hiddenInfo').animate({opacity : "0"}, 300)
-
+            // Show info block that was just clicked
             $(this).addClass('active');
             $(this).find('.aboutLink-block-wrapper').animate({
                 opacity: '0',
-            }, 300);
+            }, 300, function(){
+                $(this).css('display', 'none');
+            });
             $(this).find('.hiddenInfo').animate({opacity: '1'}, 300);
         }
         else{
+            // Hise that block
             $(this).removeClass('active');
             $(this).find('.aboutLink-block-wrapper').animate({
                 opacity: '1',
-            }, 300);
+            }, 300, function(){
+                $(this).css('display', '');
+            });
             $(this).find('.hiddenInfo').animate({opacity: '0'}, 300);
         }
     });
-
-
+    //Hide if clicked not on info block
+    $(document).on('click', function(event) {
+        var target = $(event.target);
+        if (!target.closest('.aboutLink-block').length) {
+            // Hide all info blocks
+            $('.aboutLink-block').removeClass('active');
+            $('.aboutLink-block').find('.aboutLink-block-wrapper').animate({
+                opacity: '1',
+            }, 300, function(){
+                $(this).css('display', 'block');
+            });
+            $('.hiddenInfo').animate({opacity : "0"}, 300)
+        }
+      });
+    //Show next info block
+    $('.button-right').click(function(){
+        var actualHiddenInfo = $(this).closest('.aboutLink-block').find('.hiddenInfo').attr('id');
+        if(actualHiddenInfo == 'js-hiddenInfo-1'){
+            var nextInfoBlock = '#js-hiddenInfo-2'
+            $(nextInfoBlock).closest('.aboutLink-block').addClass('active');
+            $(nextInfoBlock).closest('.aboutLink-block').find('.aboutLink-block-wrapper').animate({
+                opacity: '0',
+            }, 300, function(){
+                $(this).css('display', 'none');
+            });
+            $(nextInfoBlock).closest('.aboutLink-block').find('.hiddenInfo').animate({opacity: '1'}, 300);
+        }
+        else if(actualHiddenInfo == 'js-hiddenInfo-2'){
+            var nextInfoBlock = '#js-hiddenInfo-3'
+            $(nextInfoBlock).closest('.aboutLink-block').addClass('active');
+            $(nextInfoBlock).closest('.aboutLink-block').find('.aboutLink-block-wrapper').animate({
+                opacity: '0',
+            }, 300, function(){
+                $(this).css('display', 'none');
+            });
+            $(nextInfoBlock).closest('.aboutLink-block').find('.hiddenInfo').animate({opacity: '1'}, 300);
+        }
+        else if(actualHiddenInfo == 'js-hiddenInfo-3'){
+            var nextInfoBlock = '#js-hiddenInfo-4'
+            $(nextInfoBlock).closest('.aboutLink-block').addClass('active');
+            $(nextInfoBlock).closest('.aboutLink-block').find('.aboutLink-block-wrapper').animate({
+                opacity: '0',
+            }, 300, function(){
+                $(this).css('display', 'none');
+            });
+            $(nextInfoBlock).closest('.aboutLink-block').find('.hiddenInfo').animate({opacity: '1'}, 300);
+        }
+        else if(actualHiddenInfo == 'js-hiddenInfo-4'){
+            var nextInfoBlock = '#js-hiddenInfo-1'
+            $(nextInfoBlock).closest('.aboutLink-block').addClass('active');
+            $(nextInfoBlock).closest('.aboutLink-block').find('.aboutLink-block-wrapper').animate({
+                opacity: '0',
+            }, 300, function(){
+                $(this).css('display', 'none');
+            });
+            $(nextInfoBlock).closest('.aboutLink-block').find('.hiddenInfo').animate({opacity: '1'}, 300);
+        }
+    });
+    //Show previous info block
+    $('.button-left').click(function(){
+        var actualHiddenInfo = $(this).closest('.aboutLink-block').find('.hiddenInfo').attr('id');
+        if(actualHiddenInfo == 'js-hiddenInfo-1'){
+            var nextInfoBlock = '#js-hiddenInfo-4'
+            $(nextInfoBlock).closest('.aboutLink-block').addClass('active');
+            $(nextInfoBlock).closest('.aboutLink-block').find('.aboutLink-block-wrapper').animate({
+                opacity: '0',
+            }, 300, function(){
+                $(this).css('display', 'none');
+            });
+            $(nextInfoBlock).closest('.aboutLink-block').find('.hiddenInfo').animate({opacity: '1'}, 300);
+        }
+        else if(actualHiddenInfo == 'js-hiddenInfo-4'){
+            var nextInfoBlock = '#js-hiddenInfo-3'
+            $(nextInfoBlock).closest('.aboutLink-block').addClass('active');
+            $(nextInfoBlock).closest('.aboutLink-block').find('.aboutLink-block-wrapper').animate({
+                opacity: '0',
+            }, 300, function(){
+                $(this).css('display', 'none');
+            });
+            $(nextInfoBlock).closest('.aboutLink-block').find('.hiddenInfo').animate({opacity: '1'}, 300);
+        }
+        else if(actualHiddenInfo == 'js-hiddenInfo-3'){
+            var nextInfoBlock = '#js-hiddenInfo-2'
+            $(nextInfoBlock).closest('.aboutLink-block').addClass('active');
+            $(nextInfoBlock).closest('.aboutLink-block').find('.aboutLink-block-wrapper').animate({
+                opacity: '0',
+            }, 300, function(){
+                $(this).css('display', 'none');
+            });
+            $(nextInfoBlock).closest('.aboutLink-block').find('.hiddenInfo').animate({opacity: '1'}, 300);
+        }
+        else if(actualHiddenInfo == 'js-hiddenInfo-2'){
+            var nextInfoBlock = '#js-hiddenInfo-1'
+            $(nextInfoBlock).closest('.aboutLink-block').addClass('active');
+            $(nextInfoBlock).closest('.aboutLink-block').find('.aboutLink-block-wrapper').animate({
+                opacity: '0',
+            }, 300, function(){
+                $(this).css('display', 'none');
+            });
+            $(nextInfoBlock).closest('.aboutLink-block').find('.hiddenInfo').animate({opacity: '1'}, 300);
+        }
+    });
 });
 
 
